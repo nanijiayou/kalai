c422d9176c9dd3506c649f9406c98ecc8a2e273c
nanijiayou <heyuanjiayou@gmail.com>
2016-07-17 21:07:25 +0800

    kalai

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Search, Slider, SelectSlider, DateRange } from "./index";
+import { Search } from "./index";
 
 export default class Test extends React.Component{
     constructor(props) {
@@ -16,13 +16,6 @@ export default class Test extends React.Component{
             <div>
                 <h1>搜索</h1>
                 <Search />
-                <h1>slider</h1>
-                <h2>normal slider</h2>
-                <Slider />
-                <h2>select slider</h2>
-                <SelectSlider name="购物中心" type="mall"/>
-                <h1>日期组建</h1>
-                <DateRange />
             </div>
         )
     }
