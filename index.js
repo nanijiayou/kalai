705dc431a6a37a7dccdfacb3c4780816eed92064
nanijiayou <heyuanjiayou@gmail.com>
2016-07-26 19:53:11 +0800

    kalia

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import Search from './components/Search/Search';
 export {Search};
 
-// import Slider from './components/Slider/Slider';
-// export {Slider};
-//
-// import SelectSlider from './components/Slider/SelectSlider';
-// export {SelectSlider}
-//
-// import DateRange from './components/DateRange/DateRange';
-// export {DateRange};
+import Slider from './components/Slider/Slider';
+export {Slider};
+
+import SelectSlider from './components/Slider/SelectSlider';
+export {SelectSlider}
+
+import DateRange from './components/DateRange/DateRange';
+export {DateRange};
